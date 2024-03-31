8767e07eb633beff7a0ad5cdc8306b6294358f0c
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-03-31 13:58:37 +0200

    nest passport jwt and async modules

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,6 +3,10 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+// import { JwtModule } from '@nestjs/jwt';
 
 
 @Module({
@@ -10,8 +14,22 @@ import { User } from './entities/user.entity';
   providers: [AuthService],
   imports: [
     TypeOrmModule.forFeature([User]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '2h',
+          },
+        };
+      }
+    })
     
-  ],
+],
   exports: [TypeOrmModule] // Exporta el módulo TypeOrmModule para que pueda ser usado en otros módulos. Esto permite que los módulos que lo requieran puedan acceder a la base de datos.
 })
 export class AuthModule {}
