7243bf6bc197610ce46082659f1b24fc33a82dfb
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-03-31 15:39:50 +0200

    custom decorators

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from './decorators/get-use.decorator';
+import { User } from './entities/user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -18,10 +20,18 @@ export class AuthController {
 
   @Get('private')
   @UseGuards(AuthGuard())
-  testingPrivateRoute() {
+  testingPrivateRoute(
+    // @Req() request: Express.Request
+    @GetUser() user: User
+    ) {
+    // console.log({user: request.user});
+    
+    
+    
     return {
       ok: true,
       message: 'Hola mundo Private route',
+      user
     };
   }
 }
