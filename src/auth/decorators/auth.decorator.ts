ee6e06a645255f65a672daac53ae493ae086ea01
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-04-05 18:03:49 +0200

    refactor

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -4,11 +4,11 @@ import { RoleProtected } from './role-protected.decorator';
 import { AuthGuard } from '@nestjs/passport';
 import { UserRoleGuard } from '../guards/user-role.guard';
 
+//Este decorador implementa el RoleProtected para saber si un usuario esta logueado  y tiene los roles que se le pasan mediante la interface tambien llamamos al useguard para que valide si el usuario existe o no
 
 export function Auth(...roles: ValidRoles[]) {
   return applyDecorators(
     RoleProtected(...roles),
     UseGuards(AuthGuard(), UserRoleGuard),
-    
   );
 }
