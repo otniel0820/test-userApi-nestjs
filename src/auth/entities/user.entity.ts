dfc826b9468e852c7317934040114821526e9b0d
Otniel Lascano dev <otniel.lascano.dev@proton.me>
2024-04-07 12:49:52 +0200

    refactor

--- src/auth/entities/user.entity.ts
+++ src/auth/entities/user.entity.ts
@@ -1,43 +1,47 @@
-import { BeforeInsert, BeforeUpdate, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
-
+import {
+  BeforeInsert,
+  BeforeUpdate,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
 
 @Entity('users')
 export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
-    @PrimaryGeneratedColumn('uuid')
-    id: string;
+  @Column('text', {
+    unique: true,
+  })
+  email: string;
 
-    @Column('text',{
-        unique: true
-    })
-    email: string;
+  @Column('text', {
+    select: false, // Esto es para que la contraseña no se muestre en la respuesta de la api
+  })
+  password: string;
 
-    @Column('text', {
-        select: false // Esto es para que la contraseña no se muestre en la respuesta de la api
-    })
-    password: string;
+  @Column('text')
+  fullName: string;
 
-    @Column('text')
-    fullName: string;
+  @Column('bool', {
+    default: true,
+  })
+  isActive: boolean;
 
-    @Column('bool', {
-        default: true
-    })
-    isActive: boolean;
+  @Column('text', {
+    array: true,
+    default: ['user'], // Valor por defecto para el campo roles.
+  })
+  roles: string[];
 
-    @Column('text',{
-        array: true,
-        default: ['user'] // Valor por defecto para el campo roles.
-    })
-    roles: string[];
+  @BeforeInsert()
+  checkFieldsBeforeInsert() {
+    this.email = this.email.toLowerCase().trim(); //convertir a minusculas y quitar espacios en blanco.
+  }
 
-    @BeforeInsert()
-    checkFieldsBeforeInsert(){
-        this.email= this.email.toLowerCase().trim(); //convertir a minusculas y quitar espacios en blanco.
-    }
-
-    @BeforeUpdate()
-    checkFieldForUpdate(){
-        this.checkFieldsBeforeInsert();
-    }
+  @BeforeUpdate()
+  checkFieldForUpdate() {
+    this.checkFieldsBeforeInsert();
+  }
 }
